f033e98fa542a23f066e7238b7c519bdf41a40a8
phucnguyen02 <ndhp02@gmail.com>
2023-11-10 11:25:52 -0500

    N queens

--- n_queens.py
+++ n_queens.py
@@ -2,17 +2,18 @@ from math import *
 
 class Solution:
     def n_queens(self, n):
+        if not n: return []
+        if n == 1: return [['Q']]
         res = []
         board = ['.'*n for i in range(n)]
         col = [False]*n
-        diag1 = [False]*(n ** 2)
-        diag2 = [False]*(n ** 2)
+        diag1 = [False]*(2*n)
+        diag2 = [False]*(2*n)
         def backtracking(r):
             if r == n:
                 res.append(board[:][:])
                 return
             for c in range(n):
-                #print(r, c, diag1, diag2)
                 if col[c] or diag1[r + c] or diag2[r-c+n-1]: continue
                 col[c] = diag1[r+c] = diag2[r-c+n-1] = True
                 cur_row = list(board[r])
@@ -27,7 +28,32 @@ class Solution:
 
 if __name__ == "__main__":
     sol = Solution()
+    print(sol.n_queens(0))
+    print(sol.n_queens(1))
     print(sol.n_queens(3))
     print(sol.n_queens(4))
+    print(sol.n_queens(5))
 
-#Preorder traversal = check left branch -> check right branch -> check current node val
+# Attempt to place a queen in the first row, see if you can place queens in other rows based on that data
+# Checks for valid position with a column array and 2 diagonal arrays.
+# If a queen occupies a position, that column and corresponding diagonals are also marked as occupied.
+# A solution is valid if n queens are placed into n rows
+
+# res = []
+# col = [False]*n
+# diag1 = [False]*(2*n)
+# diag2 = [False]*(2*n)
+# backtrack(r):
+#   if r == n:
+#       append board into res
+#       return
+#   for c = 0, ..., n-1
+#       if board[r][c] is not valid then continue
+#       col[c] = diag1[r + c] = diag2[r - c + n - 1] = True
+#       board[r][c] = 'Q'
+#       backtrack(r + 1)
+#       board[r][c] = '.'
+#       col[c] = diag1[r + c] = diag2[r - c + n - 1] = True
+#
+# backtrack(0) 
+# return res
