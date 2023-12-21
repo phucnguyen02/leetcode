bcc6e8ff00bfc6c837c83876e70c8f2c0dc0e7d4
Phuc Nguyen (Brav) <ndhp02@gmail.com>
2023-12-21 01:00:25 -0500

    Buy stock 2

---
+++ 122_buy_stock_2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxProfit(self, prices) -> int:
+        n = len(prices)
+        if n == 1: return 0
+        if n == 2: return max(0, prices[1] - prices[0])
+        dp = [0]*n
+        for i in range(1, n):
+            dp[i] = dp[i-1]
+            if prices[i] > prices[i-1]:
+                dp[i] += (prices[i] - prices[i-1])
+        return dp[-1]
+    
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.maxProfit([7,1,5,3,6,4]))
+    print(sol.maxProfit([7,6,4,3,1]))
+    
+
+# Variables: dp(i) being the maximum profit on day i.
+    
+# Base case: dp(0) = 0
+    
+# Recurrence relation:
+    # dp(i) = dp(i - 1) 
+    # dp(i) += prices[i] - prices[i-1] if prices[i] > prices[i-1]
+
+# The best solution is to buy the lowest stock at a certain point and eventually sell it if it reaches its peak.
+# For example, [1, 5, 9, 3, 7] means you buy the stock at 1 and sell it at 9 and then buy it at 3 again and sell it at 7.
+
+    
