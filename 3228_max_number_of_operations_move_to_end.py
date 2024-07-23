25ffd50c2e9b7fca7bfe12e4da6a63f72eebc11c
phucnguyen02 <ndhp02@gmail.com>
2024-07-22 18:54:37 -0700

    test

--- 3228_max_number_of_operations_move_to_end.py
+++ 3228_max_number_of_operations_move_to_end.py
@@ -17,3 +17,5 @@ class Solution:
 # If we encounter a 1 after going through some number of 0s, we can maximize the number of operations by moving every 1 before that in order from right to left
 # For example, 111001, we encounter the 1 at i = 5, so we keep track of the number of 1s we saw before that and add it to our operations counter.
 # We add a 1 to the end of the original string to cover the case in which the last digit is a 0, that way we get in any remaining operations
+
+#test
